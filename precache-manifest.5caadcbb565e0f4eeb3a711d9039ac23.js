49b6448f47c3f218a6b696b76fe9ac561ca2e18d
unknown <leoff152@gmail.com>
2020-10-14 13:33:26 +0200

    Updates

---
+++ precache-manifest.5caadcbb565e0f4eeb3a711d9039ac23.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb455e6d23f02e71798a48481915a5e4",
+    "url": "/dota-drafter/index.html"
+  },
+  {
+    "revision": "0020402376b9abfe14ab",
+    "url": "/dota-drafter/static/css/2.25c43c50.chunk.css"
+  },
+  {
+    "revision": "ef417a6e2460cfbad260",
+    "url": "/dota-drafter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0020402376b9abfe14ab",
+    "url": "/dota-drafter/static/js/2.8be678e0.chunk.js"
+  },
+  {
+    "revision": "f7f850d86e26182c99733ef566cd9951",
+    "url": "/dota-drafter/static/js/2.8be678e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef417a6e2460cfbad260",
+    "url": "/dota-drafter/static/js/main.74111025.chunk.js"
+  },
+  {
+    "revision": "728bfbcb6b9635539692",
+    "url": "/dota-drafter/static/js/runtime-main.e3ff7079.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/dota-drafter/static/media/md-icons.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/dota-drafter/static/media/md-icons.570eb838.woff2"
+  },
+  {
+    "revision": "17c283b4e785e073ec09dc72acebafac",
+    "url": "/dota-drafter/static/media/open-sans-bold.17c283b4.woff2"
+  },
+  {
+    "revision": "213e8c892aa1af1d690179b37b53e35a",
+    "url": "/dota-drafter/static/media/open-sans-bold.213e8c89.woff"
+  },
+  {
+    "revision": "a9557eb451f17dcd8e687327ea9383a0",
+    "url": "/dota-drafter/static/media/open-sans-regular.a9557eb4.woff2"
+  },
+  {
+    "revision": "e15b8743b41a23082780aee5ee1ee9a9",
+    "url": "/dota-drafter/static/media/open-sans-regular.e15b8743.woff"
+  },
+  {
+    "revision": "5d001a17da29c92d711530e2ee617f7e",
+    "url": "/dota-drafter/static/media/open-sans-semibold.5d001a17.woff"
+  },
+  {
+    "revision": "97593b89e95959c7f41c47cf407d1f63",
+    "url": "/dota-drafter/static/media/open-sans-semibold.97593b89.woff2"
+  }
+]);
